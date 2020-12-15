db4fdb628b15fba4661bfeca56083b2f12e712e5
Julia Kim <26291536+julia-kim@users.noreply.github.com>
2020-12-15 17:25:58 -0500

    Add error handling

--- src/main/java/com/tts/MapsApp/MapService.java
+++ src/main/java/com/tts/MapsApp/MapService.java
@@ -17,27 +17,41 @@ public class MapService {
 	private String apiKey;
 
 	public void addCoordinates(Location location) {
-		String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location.getCity() + ","
-				+ location.getState() + "&key=" + apiKey;
-		RestTemplate restTemplate = new RestTemplate();
-		GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
-		Location coordinates = response.getResults().get(0).getGeometry().getLocation();
-		location.setLat(coordinates.getLat());
-		location.setLng(coordinates.getLng());
+		try {
+			String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location.getCity() + ","
+					+ location.getState() + "&key=" + apiKey;
+			RestTemplate restTemplate = new RestTemplate();
+			GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
+			Location coordinates = response.getResults().get(0).getGeometry().getLocation();
+			location.setLat(coordinates.getLat());
+			location.setLng(coordinates.getLng());
+		} catch (Exception ex) {
+			location.setLat("42.434722");
+			location.setLng("-83.985");
+			location.setCity("Hell");
+			location.setState("Michigan.  An error occured and you were sent here automatically. Try again.");
+		}
 	}
 
 	public void getRandomCoordinates(Location location) {
-		String randomLat = String.valueOf((Math.random() * 180.0) - 90.0);
-		String randomLong = String.valueOf((Math.random() * 360.0) - 180.0);
-		String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + randomLat + "," + randomLong
-				+ "&key=" + apiKey;
-		RestTemplate restTemplate = new RestTemplate();
-		GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
-		Location coordinates = response.getResults().get(0).getGeometry().getLocation();
-		location.setLat(randomLat);
-		location.setLng(randomLong);
-		location.setCity(coordinates.getCity());
-		location.setState(coordinates.getState());
-		System.out.print(location);
+		try {
+			String randomLat = String.valueOf((Math.random() * 180.0) - 90.0);
+			String randomLong = String.valueOf((Math.random() * 360.0) - 180.0);
+			String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + randomLat + "," + randomLong
+					+ "&key=" + apiKey;
+			RestTemplate restTemplate = new RestTemplate();
+			GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
+			Location coordinates = response.getResults().get(0).getGeometry().getLocation();
+			location.setLat(randomLat);
+			location.setLng(randomLong);
+			location.setCity(coordinates.getCity());
+			location.setState(coordinates.getState());
+			System.out.print(location);
+		} catch (Exception ex) {
+			location.setLat("42.434722");
+			location.setLng("-83.985");
+			location.setCity("Hell");
+			location.setState("Michigan.  An error occured and you were sent here automatically. Try again.");
+		}
 	}
 }
